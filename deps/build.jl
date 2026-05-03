f85142226bca528eebac5d85632f2e0ba75a2c13
abhro <5664668+abhro@users.noreply.github.com>
2026-05-03 14:33:29 -0400

    Reformat build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -9,19 +9,25 @@ prefix = joinpath(BinDeps.depsdir(libsofa_c), "usr")
 libdir = joinpath(BinDeps.depsdir(libsofa_c), "usr", "lib")
 srcdir = joinpath(BinDeps.depsdir(libsofa_c), "src", "sofa")
 
-provides(SimpleBuild,
-    (@build_steps begin
-    CreateDirectory(prefix)
-    CreateDirectory(libdir)
+provides(
+    SimpleBuild,
+    (
         @build_steps begin
-            ChangeDirectory(srcdir)
-            `make clean`
-            `make`
-            `cp libsofa_c.so libsofa_c.dylib` 
-            `mv libsofa_c.so $prefix/lib` 
-            `mv libsofa_c.dylib $prefix/lib` 
-            `make clean`
+            CreateDirectory(prefix)
+            CreateDirectory(libdir)
+            @build_steps begin
+                ChangeDirectory(srcdir)
+                `make clean`
+                `make`
+                `cp libsofa_c.so libsofa_c.dylib`
+                `mv libsofa_c.so $prefix/lib`
+                `mv libsofa_c.dylib $prefix/lib`
+                `make clean`
+            end
         end
-    end), libsofa_c, os = :Unix)
+    ),
+    libsofa_c,
+    os = :Unix,
+)
 
 @BinDeps.install Dict(:libsofa_c => :libsofa_c)
