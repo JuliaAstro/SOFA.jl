11903ed3d87b5f509dca0e1cf6e4117da5999337
Ian Weaver <weaveric@gmail.com>
2026-05-12 12:04:18 -0700

    build: update make file

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,24 @@
-using Documenter, SOFA
+using Documenter
+using Documenter.Remotes: GitHub
+using SOFA
 
 # Generate documents
 makedocs(
     modules   = [SOFA],
-    doctest   = false,
-    clean     = true,
-    linkcheck = true,
-    format    = Documenter.HTML(
-        size_threshold_warn = 400 * 1024,   # 200 KiB
-        size_threshold = 800 * 1024,        # 400 KiB
-    ),
-    warnonly = [:docs_block, :missing_docs],
-    sitename  = "SOFA.jl",
     authors   = "Duncan Eddy",
-    pages     = Any[
+    repo = GitHub("JuliaAstro/SOFA.jl"),
+    sitename  = "SOFA.jl",
+    format = Documenter.HTML(;
+        canonical = "https://juliaastro.org/SOFA/stable/",
+    ),
+    pages = [
         "Home" => "index.md",
         "SOFA Library" => "sofa.md",
-    ]
+    ],
 )
 
 deploydocs(
     repo = "github.com/JuliaAstro/SOFA.jl",
-    devbranch = "master",
-    devurl = "latest",
+    push_preview = true,
+    versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
 )
